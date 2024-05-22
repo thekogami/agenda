74c054399eb18fc53a8e86b3dececa9f65250860
felipemourao <felipemourao@univille.br>
2024-05-22 01:14:55 -0300

    upload Config

--- src/main/java/com/leandersonandre/agenda/config/Configuracao.java
+++ src/main/java/com/leandersonandre/agenda/config/Configuracao.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @CrossOrigin
-public class configuracao implements WebMvcConfigurer {
+public class Configuracao implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
